007d5cfe4d3076bc33ee5a1cc88db4660d7d3be7
phyziyx <96657278+phyziyx@users.noreply.github.com>
2024-02-01 19:20:16 +0500

    add: keyboard handling

--- src/KeyButton.tsx
+++ src/KeyButton.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const KeyButton = ({
 	letter,
@@ -9,13 +9,37 @@ const KeyButton = ({
 }) => {
 	const [isDisabled, setDisabled] = useState<boolean>(false);
 
+	const clickHandler = () => {
+		addLetter(letter);
+		setDisabled(true);
+	};
+
+	const keyDownHandler = (e: KeyboardEvent) => {
+		if (e.key.toLowerCase() !== letter.toLowerCase()) {
+			return;
+		}
+
+		clickHandler();
+	};
+
+	useEffect(() => {
+		document.addEventListener("keydown", keyDownHandler);
+
+		return () => {
+			document.removeEventListener("keydown", keyDownHandler);
+		};
+	});
+
 	return (
 		<button
 			key={letter}
-			onClick={() => {
-				addLetter(letter);
-				setDisabled(true);
+			onKeyDown={(e) => {
+				console.log(e.key);
+				if (e.key.toLowerCase() === letter) {
+					clickHandler();
+				}
 			}}
+			onClick={() => clickHandler()}
 			disabled={isDisabled}
 		>
 			{letter}
